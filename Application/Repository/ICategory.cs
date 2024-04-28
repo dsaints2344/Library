28c8782250478ad255e7a6ee2800591f9562be60
David De Los Santos <ddelossantos@neural.com.do>
2024-04-28 11:51:55 -0400

    Working on repositories
    Adding Category repository

---
+++ Application/Repository/ICategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repository
+{
+    public interface ICategory
+    {
+        public Task<CategoryModel> CreateCategory(CategoryModel category);
+        public Task<CategoryModel> GetCategory(int id);
+        public Task<List<CategoryModel>> GetAllCategories();
+    }
+}
