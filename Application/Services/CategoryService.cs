17d5cb3cf0f778f860e959ae0327567eb24b9767
David De Los Santos <ddelossantos@neural.com.do>
2024-04-28 12:27:07 -0400

    WIP
    Adding category service file and logic for getting categories

---
+++ Application/Services/CategoryService.cs
@@ -0,0 +1,45 @@
+﻿
+using Application.Repository;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Category
+{
+    public class CategoryService: ICategory
+    {
+        private readonly DataContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public CategoryService(DataContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+
+        public Task<CategoryModel> CreateCategory(CategoryModel category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<CategoryModel>> GetAllCategories()
+        {
+            var categories = await _dbContext.Categories
+                .ProjectTo<CategoryModel>(_mapper.ConfigurationProvider).ToListAsync();
+            
+
+            return categories;
+        }
+
+        public Task<CategoryModel> GetCategory(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
