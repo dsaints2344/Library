a87bfacc13e27bde273af38a4c8d85d9d85301c0
David De Los Santos <ddelossantos@neural.com.do>
2024-04-29 11:08:47 -0400

    Creating Inventory DTO

---
+++ Application/DTOs/InventoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs
+{
+    public class InventoryModel
+    {
+        public int BookId { get; set; }
+        public int NewAmount { get; set; } = 0;
+    }
+}
