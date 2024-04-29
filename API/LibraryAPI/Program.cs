1ea113c1db36df85a4004d119414512a858f7dea
David De Los Santos <ddelossantos@neural.com.do>
2024-04-28 23:52:40 -0400

    Creating seeding service
    This is required given that DB is in-memory

--- API/LibraryAPI/Program.cs
+++ API/LibraryAPI/Program.cs
@@ -1,3 +1,7 @@
+using Application.Category;
+using Application.Core;
+using Application.Repository;
+using LibraryAPI;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -9,10 +13,20 @@ builder.Services.AddDbContext<DataContext>(opts =>
     opts.UseInMemoryDatabase("LibraryDb");
 });
 
+builder.Services.AddAutoMapper(cfg =>
+{
+    cfg.AddProfile<MappingProfiles>();
+});
+// builder.Services.AddScoped<ICategory, CategoryService>();
 builder.Services.AddControllers();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Seeding service
+var context = builder.Services.BuildServiceProvider().GetRequiredService<DataContext>();
+SeedingService.SeedData(context).Wait();
 
 var app = builder.Build();
 
---
+++ API/LibraryAPI/SeedingService.cs
@@ -0,0 +1,49 @@
+﻿using Domain;
+using Persistence;
+
+namespace LibraryAPI
+{
+    public class SeedingService
+    {
+        private readonly DataContext _context;
+
+        public SeedingService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public static async Task SeedData(DataContext context)
+        {
+            var categories = new Category[] {
+                new Category { Id = 1, Name = "Science Fiction" },
+                new Category { Id = 2, Name = "Fantasy" }
+            };
+
+            await context.Categories.AddRangeAsync(categories);
+
+            var books = new Book[] {
+                new Book {
+                    Id = 1,
+                    Title = "The Hitchhiker's Guide to the Galaxy",
+                    Author = "Douglas Adams",
+                    PublishedDate = DateTime.UtcNow,
+                    ISBN = "978-0345391803",
+                    CategoryId = 1,
+                    ImgUrl = "https://en.wikipedia.org/wiki/The_Hitchhiker%27s_Guide_to_the_Galaxy_%28fictional%29"
+                },
+                new Book {
+                    Id = 2,
+                    Title = "The Lord of the Rings",
+                    Author = "J.R.R. Tolkien",
+                    PublishedDate = DateTime.UtcNow,
+                    ISBN = "978-0618219731",
+                    CategoryId = 2,
+                    ImgUrl = "https://en.wikipedia.org/wiki/Category:J._R._R._Tolkien_book_cover_images"
+                }
+            };
+
+            await context.Books.AddRangeAsync(books);
+            await context.SaveChangesAsync();
+        }
+    }
+}
