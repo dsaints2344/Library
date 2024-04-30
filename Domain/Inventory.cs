3f88f6c2d795c163a64161c759fdffc8d61168dc
David De Los Santos <ddelossantos@neural.com.do>
2024-04-29 22:16:29 -0400

    Updatign inventory model

--- Domain/Inventory.cs
+++ Domain/Inventory.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public required int BookId { get; set; }
-        public required Book Book { get; set; }
+        public Book? Book { get; set; }
         public int AvailableCopies { get; set; } = 0;
     }
 }
